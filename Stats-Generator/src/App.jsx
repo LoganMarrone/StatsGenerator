8af7dc6b3de9296552a57ca9c9b2b7f87da14d9a
Logan Marrone <lmarrone2021@fau.edu>
2024-03-08 23:07:42 -0500

    Create Roll Mechanic for Stats and more!

--- Stats-Generator/src/App.jsx
+++ Stats-Generator/src/App.jsx
@@ -4,14 +4,15 @@ import './Stats.jsx'
 import Stats from './Stats.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [classes, setClass] = useState("artificer");
+  const [levels, setLevel] = useState("1");
 
   return (
     <>
       <div>
       <h1>Table Top RPG Stats Generator</h1>
-      <label for="class">Select a Class:</label>
-      <select name="class" id="class">
+      <label htmlFor="class">Select a Class:</label>
+      <select name="class" id="class" value={classes} onChange={(e) => setClass(e.target.value)}>
         <option value="artificer">Artificer</option>
         <option value="barbarian">Barbarian</option>
         <option value="bard">Bard</option>
@@ -25,6 +26,30 @@ function App() {
         <option value="sorcerer">Sorcerer</option>
         <option value="warlock">Warlock</option>
         <option value="wizard">Wizard</option>
+      </select>
+      <br></br>
+      <label htmlFor="level">Select a Level:</label>
+      <select name="level" id="level" value={levels} onChange={(e) => setLevel(e.target.value)}>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12">12</option>
+        <option value="13">13</option>
+        <option value="14">14</option>
+        <option value="15">15</option>
+        <option value="16">16</option>
+        <option value="17">17</option>
+        <option value="18">18</option>
+        <option value="19">19</option>
+        <option value="20">20</option>
       </select>
       <Stats></Stats>
       </div>
--- Stats-Generator/src/Stats.jsx
+++ Stats-Generator/src/Stats.jsx
@@ -1,7 +1,55 @@
 import './Stats.css'
+import React, { useState } from 'react';
 
 function Stats(){
+    const [strength, setStrength] = useState(null);
+        const [dexterity, setDexterity] = useState(null);
+        const [constitution, setConstitution] = useState(null);
+        const [intelligence, setIntelligence] = useState(null);
+        const [wisdom, setWisdom] = useState(null);
+        const [charisma, setCharisma] = useState(null);
+
+    const clearStats = () => {
+        setStrength(null);
+        setDexterity(null);
+        setConstitution(null);
+        setIntelligence(null);
+        setWisdom(null);
+        setCharisma(null);
+    }
+    const roll = () => {
+        const rolls = [];
+        for (let index = 0; index < 6; index++){
+            rolls.push(Math.floor(Math.random() * 6) + 1);
+        }
+        rolls.sort((a,b) => b - a);
+        const highest = rolls.slice(0,3);
+        let total = 0;
+        for (let index = 0; index < 3; index++){
+            total += highest[index];
+        }
+        return total;
+    }
+    const rollStats = () => {
+        clearStats();
+        const rollStrength = roll();
+        const rollDexterity = roll();
+        const rollConstitution = roll();
+        const rollIntelligence = roll();
+        const rollWisdom = roll();
+        const rollCharisma = roll();
+        setStrength([rollStrength]);
+        setDexterity([rollDexterity]);
+        setConstitution([rollConstitution]);
+        setIntelligence([rollIntelligence]);
+        setWisdom([rollWisdom]);
+        setCharisma([rollCharisma]);
+    };
+
     return(
+        <div>
+        <br></br>
+        <button onClick={rollStats}>Roll Stats!</button>
         <table>
             <thead>
                 <th>Health</th>
@@ -15,16 +63,17 @@ function Stats(){
             <tbody>
                 <tr>
                 <td>20</td>
-                <td>20</td>
-                <td>20</td>
-                <td>20</td>
-                <td>20</td>
-                <td>20</td>
-                <td>20</td>
+                <td>{strength}</td>
+                <td>{dexterity}</td>
+                <td>{constitution}</td>
+                <td>{intelligence}</td>
+                <td>{wisdom}</td>
+                <td>{charisma}</td>
                 </tr>
             </tbody>
         </table>
+        </div>
     )
 }
 
-export default Stats
+export default Stats;
